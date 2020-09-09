2a48102bdfc13cd04bdba6ea1ab96ca9b4622ed8
MadhaviKancharla <66822759+MadhaviKancharla@users.noreply.github.com>
2020-09-09 23:22:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 var i = document.getElementById("intro");
 var j = document.getElementsByClassName("instructions");
 var k = document.getElementsByClassName("selection");
-function stop(){
-   j[0].style.opacity = "0";
-   k[0].style.opacity = "0";
-   setTimeout(function () {
-        i.style.opacity = "0";
-        j[0].style.opacity = "1";
-        k[0].style.opacity = "1";
-       
-    }, 7000);
-   
+function stop() {
+  j[0].style.opacity = "0";
+  k[0].style.opacity = "0";
+  setTimeout(function () {
+    i.style.opacity = "0";
+    j[0].style.opacity = "1";
+    k[0].style.opacity = "1";
+  }, 7000);
 }
-function dimension(a){
-   
+function dimension(a) {
+  localStorage.setItem("ID", a);
  
-    localStorage.setItem("ID",a);
-  
-   // alert("hloo");
 }
