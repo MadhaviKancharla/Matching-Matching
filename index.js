163ee77f5a83b0c49385006f178abbbdffe72ff6
MadhaviKancharla <66822759+MadhaviKancharla@users.noreply.github.com>
2020-09-09 23:11:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var i = document.getElementById("intro");
-
+var j = document.getElementsByClassName("instructions");
+var k = document.getElementsByClassName("selection");
 function stop(){
-  
-    setTimeout(function () {
-        i.style.opacity = "0";
+   j[0].style.opacity = "0";
+   k[0].style.opacity = "0";
+   setTimeout(function () {
+        i.style.opacity = "0";'
+      var j = document.getElementsByClassName("instructions");
+       var k = document.getElementsByClassName("selection");
        
     }, 7000);
    
